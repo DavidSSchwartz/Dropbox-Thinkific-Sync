81f1a0ea3321d78ebae807aba8202f01516bd6c1
David <davsschwartz@yahoo.com>
2026-01-22 12:33:28 +0200

    add right url

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const thinkific = axios.create({
-  baseURL: `https://${process.env.THINKIFIC_SUBDOMAIN}.thinkific.com/api/public/v2`,
+  baseURL: "https://api.thinkific.com/api/public/v2",
   headers: {
     "X-Auth-API-Key": process.env.THINKIFIC_API_KEY,
     "X-Auth-Subdomain": process.env.THINKIFIC_SUBDOMAIN,
