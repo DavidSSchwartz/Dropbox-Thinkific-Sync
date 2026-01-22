064df9dc5453f1b7d64e345335a07cd344026682
David <davsschwartz@yahoo.com>
2026-01-22 12:34:20 +0200

    add right urlmore

--- client.js
+++ client.js
@@ -6,7 +6,9 @@ export const thinkific = axios.create({
     "X-Auth-API-Key": process.env.THINKIFIC_API_KEY,
     "X-Auth-Subdomain": process.env.THINKIFIC_SUBDOMAIN,
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
+  timeout: 10000,
 });
 
 // const BASE = `https://${process.env.THINKIFIC_SUBDOMAIN}.thinkific.com/api/public/v2`;
