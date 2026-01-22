9433ff2428010fa1cca6aeeab282e58ed9820155
David <davsschwartz@yahoo.com>
2026-01-22 13:10:50 +0200

    correct thinkific api version

--- client.js
+++ client.js
@@ -1,14 +1,26 @@
 import axios from "axios";
 
 export const thinkific = axios.create({
-  baseURL: "https://api.thinkific.com/api/public/v2",
+  baseURL: "https://api.thinkific.com/api/public/v1",
   headers: {
     Authorization: `Bearer ${process.env.THINKIFIC_API_KEY}`,
     "X-Auth-Subdomain": process.env.THINKIFIC_SUBDOMAIN,
-    "Content-Type": "application/json",
     Accept: "application/json",
+    "Content-Type": "application/json",
   },
   timeout: 10000,
 });
 
 // const BASE = `https://${process.env.THINKIFIC_SUBDOMAIN}.thinkific.com/api/public/v2`;
+// async function a() {
+//   console.log(process.env.THINKIFIC_API_KEY);
+//   let c = await axios.get("https://api.thinkific.com/api/public/v1/courses", {
+//     headers: {
+//       Authorization: `Bearer ${process.env.THINKIFIC_API_KEY}`,
+//       Accept: "application/json",
+//       "X-Auth-Subdomain": process.env.THINKIFIC_SUBDOMAIN, // Add this!
+//     },
+//   });
+//   console.log(c);
+// }
+// a();
